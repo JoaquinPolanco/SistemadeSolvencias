68f7bb9bf486706b34b69d3e8332dd6dcd775262
Kevin Ocotan <kaocotan@gmail.com>
2023-12-05 16:36:21 -0600

    corregido cogido trash del script de las bibliotecas

--- imprimir.php
+++ imprimir.php
@@ -32,24 +32,8 @@
 http://www.catalogo.catolica.edu.sv/cgi-bin/koha/Solvencia/
 /OPAC/Solvencia/
 -->
-    <script>
-    function myFunctionBMC() {
-        document.getElementById("responsable").innerHTML = "Msc. Celina del Rosario Baires";
-        document.getElementById("cargo").innerHTML = "Directora de la Biblioteca Miguel de Cervantes";
-        document.getElementById("sede").innerHTML = "Biblioteca Miguel de Cervantes";
-        document.getElementById('imprimir').disabled = false;
-        document.getElementById('firma').src = "http://bibliotecaunicaes.catolica.edu.sv/wp-content/uploads/2020/06/FirmaCelina.png";
-        document.getElementById('sello').src = "http://bibliotecaunicaes.catolica.edu.sv/wp-content/uploads/2020/06/SelloBMC.png";
-    }
-    function myFunctionBRI() {
-      document.getElementById("responsable").innerHTML = "Msc. Rolando Geovanni Ramírez Contreras";
-      document.getElementById("cargo").innerHTML = "Director de la Biblioteca Ilobasco";
-      document.getElementById("sede").innerHTML = "Biblioteca Regional de Ilobasco";
-      document.getElementById('imprimir').disabled = false;
-      document.getElementById('firma').src = "http://bibliotecaunicaes.catolica.edu.sv/wp-content/uploads/2020/06/FirmaRolando.png";
-      document.getElementById('sello').src = "http://bibliotecaunicaes.catolica.edu.sv/wp-content/uploads/2020/06/SelloBRI.png";
-    }
-    </script>
+<script src="js/bibliotecas.js"></script>
+
           <!--[if lt IE 9]>
 
         <script type="text/javascript"> no se para que sirve
