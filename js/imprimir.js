6612bdb0829c776ee3e7e48def942b3650d3dcaf
Kevin Ocotan <kaocotan@gmail.com>
2023-12-06 16:17:49 -0600

    creacion de un archivo js para imprimir, y correcion en la llamada de la bd y header

---
+++ js/imprimir.js
@@ -0,0 +1,8 @@
+function imprSelec(muestra) {
+  var ficha = document.getElementById(muestra);
+  var ventimp = window.open(" ", "popimpr");
+  ventimp.document.write(ficha.innerHTML);
+  ventimp.document.close();
+  ventimp.print();
+  ventimp.close();
+}
